f8af0287c26e9fa9a06a3b906085d3d979847445
Štěpánka <Doma@Stepanka-MacBook-Air.local>
2025-05-31 18:10:58 +0200

    vytvoření api, částečné zpracování úkolu 16

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,34 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
+import { Joke } from '../../components/Joke/joke.jsx'
 
 export const HomePage = () => {
-  useEffect (() => {
-    import('./index.js');
-  }, []);
+const [users, setUsers] = useState([])
+
+useEffect(()=> {
+  const results = async () => {
+    const response = await fetch('http://localhost:4000/api/user')
+    const json = await response.json()
+    setUsers(json.data)
+  }
+  results()
+}, [])
 
   return (
-<div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
-          <span id="likes-up" className="likes-count likes-count--up">0</span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
-          <span id="likes-down" className="likes-count likes-count--down">0</span>
-        </div>
-      </div>
-    </div>
+<>
+    {users.map(user => (
+      <Joke
+      key={user.id}
+      userAvatar={user.avatar}
+      userName={user.name}
+      text={user.text}
+      likes={user.likes}
+      dislikes={user.dislikes}
+      />
+    ))}
+</>
   );
 }
 
